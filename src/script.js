ac694a0ef0b03b03a80ee42928be138ad343acaf
Dave Adams <46465562+davvvead@users.noreply.github.com>
2026-04-19 23:54:33 -0400

    Fix JavaScript ReferenceError identified via DevTools

--- src/script.js
+++ src/script.js
@@ -1,7 +1,10 @@
 let projectName = "WDDM 126 Final Project";
+let projectTitle = "WDDM 126 Final Project";
 
 function init() {
     console.log("Project loaded:", projectName);
 }
 
 init();
+
+console.log(projectTitle);
